a37bfe7aec2fd31d1acc1bdb63f98b722e506338
brave-course-1385 <tania@pixelsandcode.studio>
2018-11-12 22:46:53 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -8,6 +8,6 @@ function happyHolidaysTo(name){
 }
 
 
-function happyHolidaysTo(holidays, name){
-  return (`Happy ${holidays}, ${name}!`)
+function happyHolidayTo(holiday, name){
+  return (`Happy ${holiday}, ${name}!`)
 }
